95bf73f0a93f75e466239b6053a7509d9116a656
darasaikumar035 <saikumardhara923@gmail.com>
2024-04-14 13:29:54 +0530

    initaial Push

---
+++ backend/server.py
@@ -0,0 +1,71 @@
+from flask import Flask, request, jsonify
+from sql_connection import get_sql_connection
+import mysql.connector
+import json
+
+import products_dao
+import orders_dao
+import uom_dao
+
+app = Flask(__name__)
+
+connection = get_sql_connection()
+
+@app.route('/getUOM', methods=['GET'])
+def get_uom():
+    response = uom_dao.get_uoms(connection)
+    response = jsonify(response)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/getProducts', methods=['GET'])
+def get_products():
+    response = products_dao.get_all_products(connection)
+    response = jsonify(response)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/insertProduct', methods=['POST'])
+def insert_product():
+    try:
+        connection = sql_connection.get_sql_connection()
+        request_payload = request.json
+        if 'uom_id' not in request_payload or request_payload['uom_id'] is None:
+            return jsonify({'error': 'Invalid uom_id'}), 400
+        product_id = products_dao.insert_new_product(connection, request_payload)
+        return jsonify({'product_id': product_id}), 201
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    finally:
+        connection.close()
+
+
+@app.route('/getAllOrders', methods=['GET'])
+def get_all_orders():
+    response = orders_dao.get_all_orders(connection)
+    response = jsonify(response)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/insertOrder', methods=['POST'])
+def insert_order():
+    request_payload = json.loads(request.form['data'])
+    order_id = orders_dao.insert_order(connection, request_payload)
+    response = jsonify({
+        'order_id': order_id
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/deleteProduct', methods=['POST'])
+def delete_product():
+    return_id = products_dao.delete_product(connection, request.form['product_id'])
+    response = jsonify({
+        'product_id': return_id
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Grocery Store Management System")
+    app.run(port=5000)
